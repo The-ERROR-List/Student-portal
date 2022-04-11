28231431012f3c3a527e9deaa010975b2050aa6d
ibrahimalaqoul <alaqoulibrahim@gmail.com>
2022-04-11 14:41:45 +0300

    sign in

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const notFoundHandler = require('./error-handlers/404');
 const errorHandler = require('./error-handlers/500');
 const signup = require ('./routes/signup.route');
+const signin = require('./routes/signin.route');
 
 const express = require('express');
 const app = express();
@@ -11,12 +12,15 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(signup);
+app.use(signin);
 
 
 // socket.io server setup 
 // const http = require('http').Server(app);
 // const io = require('socket.io')(http);
-
+app.get('/', (req, res) => {
+    res.status(200).send('Home Route!');
+});
 
 
 
