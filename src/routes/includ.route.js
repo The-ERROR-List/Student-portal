e0a73b642d1ede8183925ea2b28f8b0b4c09df38
Mujahedyousef <mujahedyousef97@gmail.com>
2022-04-12 20:18:31 +0300

    include-route

--- src/routes/includ.route.js
+++ src/routes/includ.route.js
@@ -1,26 +1,48 @@
 "use strict";
 const router = require('express').Router();
-const {classModel} = require('../models/index.js');
-const {courseModel} = require('../models/index.js');
-const {studentModel} = require('../models/index.js');
-const {teacherModel} = require('../models/index.js');
-const {userModel} = require('../models/index.js');
+const { classModel } = require('../models/index.js');
+const { courseModel } = require('../models/index.js');
+const { studentModel } = require('../models/index.js');
+const { teacherModel } = require('../models/index.js');
+const { userModel } = require('../models/index.js');
 
-router.get('/course-teacher', getCourseTeacher);
-// router.get('/course-student', getCourseStudent);
+router.get('/course-teacher', getAllCourseTeacher);
+router.get('/user-student', getStudentUser);
 router.get('/course-classes', getCourseClasses);
 // router.get('/teacher-classes', getTeacherClasses);
 // router.get('/student-classes', getStudentClasses);
 
-async function getCourseTeacher (req,res){
-    let course
+async function getAllCourseTeacher(req, res) {
+    try {
+        let course = await courseModel.findAll({ include: [teacherModel] })
+
+        res.json(course);
+
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 }
 
+async  function getCourseClasses(req, res) {
+    try {
+        let course = await courseModel.findAll({ include: [classModel] })
+
+        res.json(course);
+
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+// async function getStudentUser(req, res) {
+//     try {
+//         let UserModel = await studentModel.findAll({ include: [userModel] })
+//         res.json(student);
 
 
-
-
-module.exports=router;
+module.exports = router;
 
 
 
