17eea0886347ab8068d00edd7fef9676efee3707
WalidAlrefai <w.alrefai.3@gmail.com>
2022-04-12 12:13:13 +0300

    acl edit

--- src/middlewares/acl.js
+++ src/middlewares/acl.js
@@ -2,16 +2,11 @@
 function acl(actions) {
     return async (req, res, next) => {
         try {
-            if(req.User.role == 'admin'){
-                if (req.User.actions.includes(actions)) {
-                    next();
-                } else {
-                    res.status(403).send('you are not allowed to access this page');
-                }
-            }else{
-                res.status(403).send('you are not admin')
+            if (req.User.actions.includes(actions)) {
+                next();
+            } else {
+                res.status(403).send('you are not allowed to access this page');
             }
-            
         } catch (error) {
             console.error(`${error}`);
             res.status(403).send('you are not allowed to access this page');
