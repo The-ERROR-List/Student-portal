41a6eb8f8f244ec2769ce3af939af27faa0fcca3
ibrahimalaqoul <alaqoulibrahim@gmail.com>
2022-04-17 20:48:27 +0300

    solved courseId in teachers model

--- src/models/teacher.model.js
+++ src/models/teacher.model.js
@@ -9,11 +9,6 @@ const Teacher = (sequelize, DataTypes) => sequelize.define('teacher', {
         type: DataTypes.UUID,
         allowNull: false,
     },
-     courseId: {
-        type: DataTypes.UUID,
-        allowNull: true,
-    },
-
     firstName: {
         type: DataTypes.STRING,
         allowNull: false,
